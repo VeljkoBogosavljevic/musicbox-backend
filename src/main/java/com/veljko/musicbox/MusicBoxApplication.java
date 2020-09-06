3a846f2a8ebb3a193c2a565379f8264ce549851d
Veljko Bogosavljevic <Veljko.Bogosavljevic@Billetnet.dk>
2020-09-06 12:34:46 +0200

    Set up MusicBox application- Spring WEB

---
+++ src/main/java/com/veljko/musicbox/MusicBoxApplication.java
@@ -0,0 +1,13 @@
+package com.veljko.musicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veljko/musicbox/MusicBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veljko.musicbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
