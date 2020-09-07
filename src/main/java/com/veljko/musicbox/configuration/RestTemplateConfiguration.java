b386073ed55312dd75cead9104a45db332c4e54e
Veljko Bogosavljevic <Veljko.Bogosavljevic@Billetnet.dk>
2020-09-08 00:49:25 +0200

    Adjust configurations

--- src/main/java/com/veljko/musicbox/configuration/RestTemplateConfiguration.java
+++ src/main/java/com/veljko/musicbox/configuration/RestTemplateConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 public final class RestTemplateConfiguration {
 	
 	private static final int CONNECT_TIMEOUT = 10000;
-	private static final int CONNECTION_REQUEST_TIMEOUT = 1000;
-	private static final int READ_TIMEOUT = 1000;
+	private static final int CONNECTION_REQUEST_TIMEOUT = 3000;
+	private static final int READ_TIMEOUT = 2000;
 	
 	private RestTemplateConfiguration () {}
 	
